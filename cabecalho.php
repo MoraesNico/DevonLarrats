b298abfebd397a90a654158c6d367091ade248c2
MoraesNico <nicolasmoraesestudo@gmail.com>
2024-08-28 16:59:02 +0000

    Construction of the header skeleton, and addition  of css style file.

--- cabecalho.php
+++ cabecalho.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Larrat's</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
-</body>
-</html>
+    <div class="header">
+        <img src="larrat.png" alt="Brand">
+        <div class="nav-links">
+            <a href="*">Link</a>
+            <a href="*">Link</a>
+            <a href="*">Link</a>
+            <a href="*">Link</a>
+            <a href="*">Link</a>
+        </div>
+    </div>
+
